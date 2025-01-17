9b7d977c0248e6f78ecde4d865b53a62a07f9f38
rdg1029 <wcle3456@gmail.com>
2025-01-17 15:29:19 +0900

    Apply favicon

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
-            // favicon: './public/favicon.png',
+            favicon: './public/favicon.png',
         }),
         new CleanWebpackPlugin(),
     ],
